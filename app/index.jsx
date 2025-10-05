dd078c61fcfda039d27742cbf8dc20ccde76e3e5
shakilshegu <muhammadshakil1968@gamil.com>
2025-10-05 19:50:06 +0530

    updated new things

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const Home = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-red-500">
+            <Text className="text-white text-lg">Home</Text>
+        </View>
+
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
